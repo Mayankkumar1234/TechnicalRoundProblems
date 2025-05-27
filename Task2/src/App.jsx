02d9be3f271ddfc23c5bb2f149817378df48ffd4
Mayankkumar1234 <mk1070355@gmail.com>
2025-05-27 13:48:21 +0530

    FirstCommit

---
+++ Task2/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react' 
+import Container from 'react-bootstrap/Container'; 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './App.css' 
+
+function App() { 
+
+  return (
+    <>
+     <Container> 
+      {/* <Row>
+        <Col>
+        Col 1</Col>
+      </Row> */}
+      Hello
+     </Container>
+    </>
+  )
+}
+
+export default App
